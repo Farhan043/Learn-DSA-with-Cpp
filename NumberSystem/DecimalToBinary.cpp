c03694cd3fd9d85f16478b801c67d1308f3df1e9
Farhan Habib <fh243901@gmail.com>
2025-06-13 18:06:25 +0530

    Added something

--- NumberSystem/DecimalToBinary.cpp
+++ NumberSystem/DecimalToBinary.cpp
@@ -1,27 +1,27 @@
 
-//This program converts a decimal number to binary using a simple algorithm.
-#include <iostream>
-using namespace std;
+// //This program converts a decimal number to binary using a simple algorithm.
+// #include <iostream>
+// using namespace std;
 
-int main() {
-    int num;
-    cout << "Enter a decimal number: ";
-    cin >> num;
+// int main() {
+//     int num;
+//     cout << "Enter a decimal number: ";
+//     cin >> num;
 
-    int binary = 0;
-    int place = 1;
+//     int binary = 0;
+//     int place = 1;
 
-    // while (num != 0) {
-    while (num > 0) {
-        int rem = num % 2;              
-        binary = binary + rem * place;  
-        place = place * 10;            
-        num = num / 2;                  
-    }
+//     // while (num != 0) {
+//     while (num > 0) {
+//         int rem = num % 2;              
+//         binary = binary + rem * place;  
+//         place = place * 10;            
+//         num = num / 2;                  
+//     }
 
-    cout << "Binary = " << binary << endl;
-    return 0;
-}
+//     cout << "Binary = " << binary << endl;
+//     return 0;
+// }
 
 
 
@@ -51,43 +51,43 @@ int main() {
 }
 
 
-// 3rd Approach: This program converts a decimal number to binary using bitset for 8-bit representation.
-#include <iostream>
-#include <bitset>
-using namespace std;
+// // 3rd Approach: This program converts a decimal number to binary using bitset for 8-bit representation.
+// #include <iostream>
+// #include <bitset>
+// using namespace std;
 
-int main() {
-    int num;
-    cout << "Enter a decimal number: ";
-    cin >> num;
+// int main() {
+//     int num;
+//     cout << "Enter a decimal number: ";
+//     cin >> num;
 
-    // Convert to 8-bit binary using two's complement
-    bitset<8> binary(num);
+//     // Convert to 8-bit binary using two's complement
+//     bitset<8> binary(num);
 
-    cout << "Binary (8-bit) = " << binary << endl;
+//     cout << "Binary (8-bit) = " << binary << endl;
 
-    return 0;
-}
+//     return 0;
+// }
 
 
 
-// 4th Approach: This program converts a decimal number to 4-bit binary using two's complement representation.
-#include <iostream>
-#include <bitset>
-using namespace std;
+// // 4th Approach: This program converts a decimal number to 4-bit binary using two's complement representation.
+// #include <iostream>
+// #include <bitset>
+// using namespace std;
 
-int main() {
-    int num;
-    cout << "Enter a decimal number (-8 to 7): ";
-    cin >> num;
+// int main() {
+//     int num;
+//     cout << "Enter a decimal number (-8 to 7): ";
+//     cin >> num;
 
-    if (num < -8 || num > 7) {
-        cout << "❌ Error: Number out of 4-bit range!" << endl;
-        return 0;
-    }
+//     if (num < -8 || num > 7) {
+//         cout << "❌ Error: Number out of 4-bit range!" << endl;
+//         return 0;
+//     }
 
-    bitset<4> binary(num); // Converts to 4-bit two's complement
-    cout << "4-bit binary = " << binary << endl;
+//     bitset<4> binary(num); // Converts to 4-bit two's complement
+//     cout << "4-bit binary = " << binary << endl;
 
-    return 0;
-}
+//     return 0;
+// }
